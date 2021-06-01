2094f77ddaadf66ea3ac575d8a15022e7594a81f
NicholasHorta <nickdeveloper.js@gmail.com>
2021-06-01 07:45:54 +0100

    Page 47 Pre 2.8

--- stocks/src/app/app.module.ts
+++ stocks/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StocksService } from './services/stocks.service';
 import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
