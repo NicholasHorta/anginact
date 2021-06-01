47e3facd0da1dd52db05f91f96cf21f3103f2201
NicholasHorta <nickdeveloper.js@gmail.com>
2021-06-01 06:25:59 +0100

    Page 44 Pre 2.7

--- stocks/src/app/app.module.ts
+++ stocks/src/app/app.module.ts
@@ -1,17 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StocksService } from './services/stocks.service';
+import { SummaryComponent } from './components/summary/summary.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SummaryComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [StocksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
