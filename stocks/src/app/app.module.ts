fac4cf31b54bcbebafde001cf49a0b750dc68e88
NicholasHorta <nickdeveloper.js@gmil.com>
2021-06-03 07:59:06 +0100

    Ch2 Completed Pre Ch3

--- stocks/src/app/app.module.ts
+++ stocks/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ManageComponent } from './components/manage/manage.component';
 import { FormsModule } from '@angular/forms';
+import { AppRoutes } from './app.routes.module';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutes
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
